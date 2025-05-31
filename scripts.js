f9286470a408a1b426c82e75f8f80bb06506e17e
hauzen-foud <willian.zanella.batista@escola.pr.gov.br>
2025-05-31 18:15:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
 
+document.querySelector('.btn-ver-mais').addEventListener('click', function() {
+    const textoOculto = document.querySelector('.texto-oculto');
+    textoOculto.style.display = textoOculto.style.display === 'none' ? 'block' : 'none';
+});
